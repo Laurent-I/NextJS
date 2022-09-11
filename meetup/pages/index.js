9d119aba2d0ba58a345eb838d5f6ae3997a9cc93
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-09-11 17:15:17 +0200

    Added static site generation (SSG)

--- meetup/pages/index.js
+++ meetup/pages/index.js
@@ -17,8 +17,18 @@ const DUMMY_MEETUPS = [
   },
 ];
 
-function HomePage() {
-  return <MeetupList meetups={DUMMY_MEETUPS} />;
+function HomePage(props) {
+  return <MeetupList meetups={props.meetups} />;
+}
+
+export async function getStaticProps() {
+  // fetch data from an API endpoint
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+    revalidate: 360000,
+  };
 }
 
 export default HomePage;
