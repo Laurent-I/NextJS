212d68890f9546ba7dd657cb8fadeac8fd1f502b
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-09-11 12:15:35 +0200

    Outputted A List of meetups

--- meetup/pages/index.js
+++ meetup/pages/index.js
@@ -1,5 +1,24 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image: "file:///C:/Users/Pc/Pictures/Saved%20Pictures/okay.jpg",
+    address: "Some address 5, 12345, Some City",
+    description: "This is a first meetup",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image: "file:///C:/Users/Pc/Pictures/Saved%20Pictures/okay.jpg",
+    address: "Some address 5, 12345, Some City",
+    description: "This is a second meetup",
+  },
+];
+
 function HomePage() {
-  return <h1>The Home Page</h1>;
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 }
 
 export default HomePage;
