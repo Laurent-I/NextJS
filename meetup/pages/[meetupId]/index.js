58e70c1eceab889d8cc9c83ea0e4f169182b6b10
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-09-11 12:53:22 +0200

    addded custom css modules and components

---
+++ meetup/pages/[meetupId]/index.js
@@ -0,0 +1,15 @@
+import { Fragment } from "react";
+
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="file:///C:/Users/Pc/Pictures/Saved%20Pictures/okay.jpg"
+      title="First Meetup Details"
+      address="Some Street 5, New York, NY, USA"
+      description="This is the first Meetup Detail"
+    />
+  );
+}
+export default MeetupDetails;
