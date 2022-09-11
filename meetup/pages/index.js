1ee082475885cd81a16950d66779f4f03d694c1f
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-09-11 18:44:46 +0200

    Fetched Data from the databasse

--- meetup/pages/index.js
+++ meetup/pages/index.js
@@ -1,21 +1,5 @@
 import MeetupList from "../components/meetups/MeetupList";
-
-const DUMMY_MEETUPS = [
-  {
-    id: "m1",
-    title: "A First Meetup",
-    image: "okay.jpg",
-    address: "Some address 5, 12345, Some City",
-    description: "This is a first meetup",
-  },
-  {
-    id: "m2",
-    title: "A Second Meetup",
-    image: "okay.jpg",
-    address: "Some address 5, 12345, Some City",
-    description: "This is a second meetup",
-  },
-];
+import { MongoClient } from "mongodb";
 
 function HomePage(props) {
   return <MeetupList meetups={props.meetups} />;
@@ -35,11 +19,27 @@ function HomePage(props) {
 
 export async function getStaticProps() {
   // fetch data from an API endpoint
+
+  const client = await MongoClient.connect("mongodb://localhost:27017/meetups");
+
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
   return {
     props: {
-      meetups: DUMMY_MEETUPS,
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
     },
-    revalidate: 360000,
+    revalidate: 360,
   };
 }
 
