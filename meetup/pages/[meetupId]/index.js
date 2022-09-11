9556c62bfc79604b3c59bf0966b7bed510f7d3bd
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-09-11 17:36:31 +0200

    Prepared paths with getStaticProps

--- meetup/pages/[meetupId]/index.js
+++ meetup/pages/[meetupId]/index.js
@@ -12,4 +12,37 @@ function MeetupDetails() {
     />
   );
 }
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  //fetch data from an API endpoint
+
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+  return {
+    props: {
+      meetupData: {
+        image: "okay.jpg",
+        id: meetupId,
+        title: "First Meetup",
+        address: "Some Street 5, New York, NY, USA",
+        description: "This is the first Meetup Detail",
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
--- meetup/pages/index.js
+++ meetup/pages/index.js
@@ -21,6 +21,18 @@ function HomePage(props) {
   return <MeetupList meetups={props.meetups} />;
 }
 
+// export async function getServerSideProps(context){
+//   const req = context.req
+//   const res = context.res
+//   //fetch data from an API endpoint
+
+//   return{
+//     props:{
+//       meetups:DUMMY_MEETUPS
+//     }
+//   }
+// }
+
 export async function getStaticProps() {
   // fetch data from an API endpoint
   return {
