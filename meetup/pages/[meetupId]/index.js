4f8b386c21260ba1f55fb2bc07c9d8d3af944e2e
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-09-11 16:51:49 +0200

    Added test image

--- meetup/pages/[meetupId]/index.js
+++ meetup/pages/[meetupId]/index.js
@@ -5,7 +5,7 @@ import MeetupDetail from "../../components/meetups/MeetupDetail";
 function MeetupDetails() {
   return (
     <MeetupDetail
-      image="file:///C:/Users/Pc/Pictures/Saved%20Pictures/okay.jpg"
+      image="okay.jpg"
       title="First Meetup Details"
       address="Some Street 5, New York, NY, USA"
       description="This is the first Meetup Detail"
--- meetup/pages/index.js
+++ meetup/pages/index.js
@@ -4,14 +4,14 @@ const DUMMY_MEETUPS = [
   {
     id: "m1",
     title: "A First Meetup",
-    image: "file:///C:/Users/Pc/Pictures/Saved%20Pictures/okay.jpg",
+    image: "okay.jpg",
     address: "Some address 5, 12345, Some City",
     description: "This is a first meetup",
   },
   {
     id: "m2",
     title: "A Second Meetup",
-    image: "file:///C:/Users/Pc/Pictures/Saved%20Pictures/okay.jpg",
+    image: "okay.jpg",
     address: "Some address 5, 12345, Some City",
     description: "This is a second meetup",
   },
