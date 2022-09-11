850f0a77d07347ae810ef74e9b4d0685f394e881
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-09-11 18:30:09 +0200

    COnnected to mongo and added add new meetup functionality

--- meetup/pages/api/new-meetup.js
+++ meetup/pages/api/new-meetup.js
@@ -1,8 +1,27 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
     const { title, image, address, description } = data;
+
+    const client = await MongoClient.connect(
+      "mongodb://localhost:27017/meetups"
+    );
+
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({
+      message: "Meetup was inserted successfully",
+    });
   }
 }
 
